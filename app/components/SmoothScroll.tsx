11b3540a590f5bdfee5b8302a934d9688adc3e33
unknown <shivaprasad26005@gmail.com>
2026-03-20 10:59:28 +0530

    fix: remove smooth dependency

--- app/components/SmoothScroll.tsx
+++ app/components/SmoothScroll.tsx
@@ -6,8 +6,7 @@ import Lenis from "@studio-freight/lenis";
 export default function SmoothScroll() {
     useEffect(() => {
         const lenis = new Lenis({
-            smooth: true,
-            lerp: 0.08, // smoothness (lower = smoother)
+            lerp: 0.08,
         });
 
         function raf(time: number) {
